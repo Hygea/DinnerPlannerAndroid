18067d4e1b2891d24c0f6380d526c26cb84b9c68
Johan B <j.blomgr@gmail.com>
2013-01-30 15:52:56 +0100

    First layout

--- gen/se/kth/csc/iprog/dinnerplanner/R.java
+++ gen/se/kth/csc/iprog/dinnerplanner/R.java
@@ -12,11 +12,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int bakedbrie=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icecream=0x7f020002;
-        public static final int meatballs=0x7f020003;
-        public static final int sourdough=0x7f020004;
-        public static final int toast=0x7f020005;
+        public static final int button_background=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icecream=0x7f020003;
+        public static final int meatballs=0x7f020004;
+        public static final int sourdough=0x7f020005;
+        public static final int toast=0x7f020006;
     }
     public static final class id {
         public static final int menu_settings=0x7f070001;
@@ -34,6 +35,9 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int newDinnerButton=0x7f040006;
+        public static final int sell=0x7f040005;
+        public static final int tda=0x7f040004;
         public static final int title_activity_choose_dish=0x7f040003;
     }
     public static final class style {
